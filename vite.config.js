0514d45eab3f9c65f1a12c4a8a22d767f451cd19
nehad222 <nihadshrith@gmail.com>
2025-02-21 08:42:15 +0000

    adsjn

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './', 
+  server: {
+    host: '0.0.0.0',  // لكي يستطيع الوصول من أي مكان
+    strictPort: false, // لا تحدد المنفذ بشكل صارم
+  },
+  preview: {
+    allowedHosts: ['repo32-3.onrender.com'], // إضافة النطاق الذي تحصل منه على التطبيق
+  },
 });
+
